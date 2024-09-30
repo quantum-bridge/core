1c5bc1d6c40ea453cbe1cc2c12c15113c9131cb1
Ruslan Yakymenko <ruslan.yakymenko17@gmail.com>
2024-09-30 11:37:23 +0300

    add native token mock

--- pkg/common/value_objects.go
+++ pkg/common/value_objects.go
@@ -2,6 +2,11 @@ package common
 
 import "golang.org/x/exp/constraints"
 
+const (
+	// NativeTokenAddress is the address of the native token.
+	NativeTokenAddress = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+)
+
 // Contains checks if a slice contains a value of type T that is ordered.
 func Contains[T constraints.Ordered](sourceSlice []T, value T) bool {
 	for _, element := range sourceSlice {
