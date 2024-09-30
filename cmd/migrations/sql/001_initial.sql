0754ddef85fa0272792e92595785cf023fdf807f
Ruslan Yakymenko <ruslan.yakymenko17@gmail.com>
2024-09-30 11:36:58 +0300

    update sql migration

--- cmd/migrations/sql/001_initial.sql
+++ cmd/migrations/sql/001_initial.sql
@@ -2,14 +2,16 @@
 create table transactions_history
 (
     id            bigserial primary key,
-    tx_hash       text      not null,
-    token_address text      not null,
+    chain_id      int     not null,
+    tx_hash       text    not null unique,
+    block_number  bigint  not null,
+    token_address text    not null,
     token_id      text,
-    amount        text      not null,
-    from_address  text      not null,
-    to_address    text      not null,
-    network_from  text      not null,
-    network_to    text      not null
+    amount        text    not null,
+    from_address  text    not null,
+    to_address    text    not null,
+    to_network    text    not null,
+    is_mintable   boolean not null
 );
 
 -- +migrate Down
