f857bf2ccaca9b62e7224ff7abad8204d89ff211
Ruslan Yakymenko <ruslan.yakymenko17@gmail.com>
2024-10-02 20:34:20 +0300

    update structure of deposits_history table

--- cmd/migrations/sql/001_deposits_history.sql
+++ cmd/migrations/sql/001_deposits_history.sql
@@ -1,32 +1,18 @@
 -- +migrate Up
 create table deposits_history
 (
-    id            bigserial primary key,
-    chain_id      int     not null,
-    tx_hash       text    not null unique,
-    block_number  bigint  not null,
-    token_address text    not null,
-    token_id      text,
-    amount        text    not null,
-    from_address  text    not null,
-    to_address    text    not null,
-    to_network    text    not null,
-    is_mintable   boolean not null
-);
-
-create table withdrawals_history
-(
-    id            bigserial primary key,
-    chain_id      int     not null,
-    tx_hash       text    not null references deposits_history(tx_hash),
-    token_address text    not null,
-    token_id      text,
-    amount        text    not null,
-    to_address    text    not null,
-    token_uri     text,
-    is_mintable   boolean not null
+    id                  bigserial primary key,
+    tx_hash             text    not null unique,
+    block_number        bigint  not null,
+    token_address       text    not null,
+    token_id            text,
+    amount              text    not null,
+    from_address        text    not null,
+    to_address          text    not null,
+    is_mintable         boolean not null,
+    source_network      text    not null,
+    destination_network text    not null
 );
 
 -- +migrate Down
 drop table deposits_history;
-drop table withdrawals_history;
