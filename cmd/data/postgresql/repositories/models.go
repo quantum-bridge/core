247d63174becb235e1c0c96d7a11fcf68d663692
Ruslan Yakymenko <ruslan.yakymenko17@gmail.com>
2024-10-12 01:04:45 +0300

    update WithdrawalsHistory model

--- cmd/data/postgresql/repositories/models.go
+++ cmd/data/postgresql/repositories/models.go
@@ -18,13 +18,12 @@ type DepositsHistory struct {
 // WithdrawalsHistory is a struct that represents the withdrawals history table.
 type WithdrawalsHistory struct {
 	ID                 int64  `db:"id" structs:"-"`
-	SourceNetwork      string `db:"source_network" structs:"source_network"`
-	TxHash             string `db:"tx_hash" structs:"tx_hash"`
+	WithdrawalTxHash   string `db:"withdrawal_tx_hash" structs:"withdrawal_tx_hash"`
+	DepositTxHash      string `db:"deposit_tx_hash" structs:"deposit_tx_hash"`
 	BlockNumber        uint64 `db:"block_number" structs:"block_number"`
 	TokenAddress       string `db:"token_address" structs:"token_address"`
 	TokenID            string `db:"token_id" structs:"token_id"`
 	Amount             string `db:"amount" structs:"amount"`
-	FromAddress        string `db:"from_address" structs:"from_address"`
 	ToAddress          string `db:"to_address" structs:"to_address"`
 	DestinationNetwork string `db:"destination_network" structs:"destination_network"`
 	IsMintable         bool   `db:"is_mintable" structs:"is_mintable"`
--- cmd/migrations/sql/002_withdrawals_history.sql
+++ cmd/migrations/sql/002_withdrawals_history.sql
@@ -2,15 +2,14 @@
 create table withdrawals_history
 (
     id                  bigserial primary key,
-    tx_hash             text    not null unique references deposits_history (tx_hash),
+    withdrawal_tx_hash  text    not null unique,
+    deposit_tx_hash     text    not null unique,
     block_number        bigint  not null,
     token_address       text    not null,
     token_id            text,
     amount              text    not null,
-    from_address        text    not null,
     to_address          text    not null,
     is_mintable         boolean not null,
-    source_network      text    not null,
     destination_network text    not null
 );
 
